71efdd75960716fab7c8f1c01b892f8a05a30c8b
Cabangon.297337 <Cabangon.297337@LAB-A-PC14>
2024-02-08 08:23:18 +0800

    Add project files.

---
+++ GreetingApp/GreetingProgram.cs
@@ -0,0 +1,21 @@
+﻿namespace GreetingApp
+{
+    internal class GreetingProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            String name = Console.ReadLine();
+
+            Console.Write("Enter the total number of your enrolled course: ");
+            int course = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the price of your favorite book: ");
+            double price = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Total enrolled course: " + course);
+            Console.WriteLine("Price of my favorite book: " + price);
+        }
+    }
+}
